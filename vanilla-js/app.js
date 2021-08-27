70818ec7986b87352bd03058c4d04a370c7c783a
junmkang <k010103@naver.com>
2021-08-27 13:04:50 +0900

    vanilla-js #3.5

--- vanilla-js/app.js
+++ vanilla-js/app.js
@@ -1,10 +1,28 @@
-const player = {
-	name: "jun",
-	points: 42,
-	fat: true,
+const h1 = document.querySelector("div.hello h1");
+
+function handTitleClick() {
+	h1.style.color = "blue";
 }
 
-console.log(player)
-console.log(player.name)
+function handleMouseEnter() {
+	h1.innerText = "Mouse is here!";
+}
 
-player.lastName = "mo"
+function handleMouseLeave() {
+	h1.innerText = "Mouse is gond!";
+}
+
+function handleWindowResize() {
+	document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+	alert("copy!");
+}
+
+h1.addEventListener("click", handTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
